15c03c4aa7f26e25eb466279acfbf905193b49ef
dougras <emaildedouglas@gmail.com>
2025-04-19 12:46:21 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NgbCalendar, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterOutlet, NgbDatepickerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'lecionario';
+  dataUnica: Date = new Date();
+
+  mudouData() {
+    alert(new Date(this.dataUnica).toUTCString())
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+// Registra os dados de localização para pt-BR
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), provideClientHydration(), 
+    provideHttpClient(withFetch()), 
+    provideAnimations(),
+    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }, // Locale configurado para Brasil
+    { provide: 'DEFAULT_TIMEZONE', useValue: 'America/Recife' }, // Fuso horário padrão
+    DatePipe]
 };
